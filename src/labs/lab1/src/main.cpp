b3f9723654c33cc5da071957e658b2f487edd87c
Timur Cravtov <timurcravtov04@gmail.com>
2026-02-02 17:16:19 +0200

    refator: separate logic of led

--- src/labs/lab1/src/main.cpp
+++ src/labs/lab1/src/main.cpp
@@ -16,46 +16,44 @@ void setup() {
   Serial.begin(9600);
   delay(100);
   
-
-  // the function does what you think it does
   redirectSerialToStdio();
 }
 
-bool equals(const char* a, const char* b) {
+bool equalsIgnoreCase(const char* a, const char* b) {
   return strcasecmp(a, b) == 0;
 }
 
 void loop() {
 
-  // reading command from serial
-  char buffer[10] = {0};
-  fflush(stdout);
+    // reading command from serial
+    char buffer[10] = {0};
+    fflush(stdout);
   
-  printf("Waiting for command (led on / led off): \n");
+    // promt to read data
+    printf("Waiting for command (led on / led off): \n");
+    readLine(buffer, sizeof(buffer));
 
-  readLine(buffer, sizeof(buffer));
+    // process command
+    if (equalsIgnoreCase(buffer, "led on")) handleLed(true);
+    else if (equalsIgnoreCase(buffer, "led off")) handleLed(false);
+    else printf("Unknown command. Use 'led on' or 'led off'.\n");
+  
+}
 
-  if (equals(buffer, "led on")) {
-
-    // printf("Intered command to turn LED ON\n");
-    if (led.isOn()) {
-      printf("LED is already ON\n");
-    } else {
-      printf("Turning LED ON\n");
-      led.on();
-    }
-  }
-
-  else if (equals(buffer, "led off")) {
-
-    // printf("Intered command to turn LED OFF\n");
+void handleLed(bool turnOn) {
+  if (turnOn) {
     if (!led.isOn()) {
-      printf("LED is already OFF\n");
+      led.on();
+      printf("LED turned ON\n");
     } else {
-      printf("Turning LED OFF\n");
-      led.off(); 
+      printf("LED is already ON\n");
     }
   } else {
-    printf("Unknown command. Use 'led on' or 'led off'.\n");
+    if (led.isOn()) {
+      led.off();
+      printf("LED turned OFF\n");
+    } else {
+      printf("LED is already OFF\n");
+    }
   }
 }
